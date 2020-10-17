b408a4bd3167ca878edf1413fb85e813a4d89bd1
Cristian Burgos <cristian.70869865@gmail.com>
2020-10-17 11:19:33 -0400

    Before workbox config

---
+++ src/components/RecipeIngredients.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class RecipeIngredients extends Component {
+  render() {
+    const { ingredients } = this.props
+    
+    return <div className="ingredients">
+      <h2>Ingredients</h2>
+      <ul>
+        { ingredients.map((i, ix) => (
+          <li key={ix}>{ i.ingredient }: { i.measure }</li>
+        )) }
+      </ul>
+    </div>
+  }
+}
---
+++ src/components/RecipeInstructions.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class RecipeInstructions extends Component {
+  render() {
+    const { instructions } = this.props
+    
+    return <div className="instructions">
+      <h2>Instructions</h2>
+
+      <div className="steps">
+        { instructions.map((line, ix) => (
+          <div className="step" key={ix}>
+            <div className="number">{ix+1}</div>
+            <div className="text">{ line }</div>
+          </div>
+        )) }
+      </div>
+    </div>
+  }
+}
